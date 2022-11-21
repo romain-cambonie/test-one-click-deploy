fab50ea31652cb60f68d52d9a812f315d1a0d730
Romain Cambonie <romain.cambonie@gmail.com>
2022-11-21 18:11:50 +0100

    Update index.ts

--- src/assets/cms-admin/index.ts
+++ src/assets/cms-admin/index.ts
@@ -2,6 +2,7 @@ import CMS from 'netlify-cms';
 import { LandingPreview } from './LandingPreview';
 
 CMS.init();
+CMS.registerPreviewStyle('https://cdn.jsdelivr.net/npm/@gouvfr/dsfr@1.8.2/dist/dsfr.min.css');
 CMS.registerPreviewTemplate('landing', LandingPreview);
 
 export default {
